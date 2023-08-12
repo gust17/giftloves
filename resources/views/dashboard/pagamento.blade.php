06aa877b845db7ab76f2a8c6eb28c36b5e17889b
gu17 <gustavopantoja.ap@gmail.com>
2023-08-12 04:40:29 -0300

    dados finais

--- resources/views/dashboard/pagamento.blade.php
+++ resources/views/dashboard/pagamento.blade.php
@@ -107,23 +107,6 @@
                 <div class="card-body p-3 pb-0">
                     <ul class="list-group">
 
-                        @forelse(auth()->user()->envios->reject(function ($envio) use ($presente) {
-     return $envio->id === $presente->id;
- }) as $envio)
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="mb-1 text-dark font-weight-bold text-sm">Março, 01, 2020</h6>
-                                    <span class="text-xs">#MS-415646</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    R$180
-                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
-                                            class="fas fa-file-pdf text-lg me-1"></i> PDF
-                                    </button>
-                                </div>
-                            </li>
-                        @empty
-                        @endforelse
 
 
                     </ul>
