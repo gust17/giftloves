8234bdb88559faee940feacf4d751297e38d9a0d
Gustavo Pantoja <gustavopantoja.ap@gmail.com>
2023-10-16 02:19:55 -0300

    automatizar pagamento e envio

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -30,22 +30,20 @@ class WebhookController extends Controller
         try {
             $data = $request->json()->all();
 
-            if ($data['event'] == 'PAYMENT_RECEIVED') {
-                $paymentId = $data['payment']['id'];
-
-                $fatura = Presente::where('asaas_id', $paymentId)->first();
-
-                if ($fatura) {
-                    $whatsappService->enviarMensagem($fatura->id);
-                    $whatsappService->enviarCode($fatura->id);
+            // Verifique se a chave 'event' existe no JSON recebido
+            if (isset($data['event'])) {
+                if ($data['event'] == 'PAYMENT_CREATED') {
+                    // Faça algo específico para 'PAYMENT_CREATED'
                 } else {
-                    // Lógica adicional em caso de fatura não encontrada (opcional)
-                    return response()->json(['message' => 'Fatura não encontrada para o pagamento.'], 404);
+                    // Qualquer outro valor de 'event' retorna um status 200 OK
+                    return response()->json(['message' => 'Event not processed, but webhook received'], 200);
                 }
+            } else {
+                // 'event' não está definido, retorne um erro 403 Forbidden
+                return response()->json(['error' => 'Event not defined'], 403);
             }
 
             return response()->json(['message' => 'Webhook received and processed'], 200);
-
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error processing the webhook'], 500);
         }
